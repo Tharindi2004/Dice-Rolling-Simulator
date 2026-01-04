903794aebe1970b1a92e66c30a176a870ac24c53
M.T.Tharindi <tharindi.20240581@iit.ac.lk>
2026-01-04 10:58:18 +0530

    Display a Dice

--- src/com/Tharindi/dice/Simulator.java
+++ src/com/Tharindi/dice/Simulator.java
@@ -2,15 +2,46 @@ package com.Tharindi.dice;
 
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Simulator {
     public static void main (String[] args) {
-        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(rand.nextInt(6)+1);
+        System.out.println("How many dice would you like to roll?: ");
+        int numberOfDice = scanner.nextInt();
+
+        System.out.println("About to roll " + numberOfDice + " dice");
 
 
+        //Random rand = new Random();
+
+        //int rolledNumber= rand.nextInt(6)+1;
+        //System.out.print(display(rolledNumber));
+
+    }
+
+    static String display (int value) {
+        switch (value) {
+            case 1:
+                return "---------\n|       |\n|   o   |\n|       |\n---------";
+            case 2:
+                return "---------\n| o     |\n|       |\n|      o|\n---------";
+            case 3:
+                return "---------\n| o     |\n|   o   |\n|      o|\n---------";
+            case 4:
+                return "---------\n| o   o |\n|       |\n|  o   o|\n---------";
+            case 5:
+                return "---------\n| o   o |\n|   o   |\n|  o   o|\n---------";
+            case 6:
+                return "---------\n| o   o |\n| o   o |\n|  o   o|\n---------";
+
+            default: return "Not a valid die value";
+        }
 
 
     }
-}
+    }
+
+
+
