30dccdfc0d0e62e807dd56552f36c3d2c28615bb
M.T.Tharindi <tharindi.20240581@iit.ac.lk>
2026-01-04 10:32:00 +0530

    Generating a random Number

---
+++ src/com/Tharindi/dice/Simulator.java
@@ -0,0 +1,16 @@
+package com.Tharindi.dice;
+
+
+import java.util.Random;
+
+public class Simulator {
+    public static void main (String[] args) {
+        Random rand = new Random();
+
+        System.out.println(rand.nextInt(6)+1);
+
+
+
+
+    }
+}
